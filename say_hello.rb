4868b0c09faf0e00fe63b876c3f961397214baa6
Jantastication O <georginaoregx@gmail.com>
2018-09-14 20:20:28 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-def say_hello(name="Ruby programmer")
-  puts "Hello #{name}!"
+def say_hello(name)
+  puts "Hello Kent Beck!"
 end
 
-def say_hello(name)
-  puts "Hello Kent Beck"
+def say_hello(name="Ruby programmer")
+  puts "Hello, #{name}!"
 end
